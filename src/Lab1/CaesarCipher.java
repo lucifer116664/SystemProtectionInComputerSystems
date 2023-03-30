2c20a3904b2f8cc13a1953f6a5ad74b4dce58c28
lucifer <lucifer116664@gmail.com>
2023-03-30 10:39:33 +0300

    Finished Lab1 (Caesar encrypter)

---
+++ src/Lab1/CaesarCipher.java
@@ -0,0 +1,61 @@
+package Lab1;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class CaesarCipher {
+    private JPanel panel;
+    private JTextField stepTextField, messageTextField, encryptedMsgTextField;
+    private JButton encryptButton;
+    private JButton clearButton;
+    private JComboBox<String> signComboBox;
+    private final String alphabet = "абвгґдеєжзиіїйклмнопрстуфхцчшщьюя";
+    public CaesarCipher() {
+        signComboBox.addItem("+");
+        signComboBox.addItem("-");
+
+        encryptButton.addActionListener(e -> {
+            encryptedMsgTextField.setText("");
+            try {
+                int step = Integer.parseInt(stepTextField.getText());
+                for (int i = 0; i < messageTextField.getText().length(); i++) {
+                    int index = alphabet.indexOf(messageTextField.getText().toCharArray()[i]);
+                    if (signComboBox.getSelectedItem() == "+") {
+                        for (int j = 0; j < step; j++) {
+                            index++;
+                            if (index == 33)
+                                index = 0;
+                        }
+                        encryptedMsgTextField.setText(encryptedMsgTextField.getText() + alphabet.toCharArray()[index]);
+                    } else {
+                        for (int j = 0; j < step; j++) {
+                            index--;
+                            if (index == -1)
+                                index = 32;
+                        }
+                        encryptedMsgTextField.setText(encryptedMsgTextField.getText() + alphabet.toCharArray()[index]);
+                    }
+                }
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(null, "You can enter only integer numbers in \"Step\" text field", "Wrong step entered", JOptionPane.WARNING_MESSAGE);
+            }
+        });
+
+        clearButton.addActionListener(e -> {
+            signComboBox.setSelectedIndex(0);
+            stepTextField.setText("");
+            messageTextField.setText("");
+            encryptedMsgTextField.setText("");
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Caesar encrypter");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setContentPane(new CaesarCipher().panel);
+
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        frame.setBounds(toolkit.getScreenSize().width / 2 - 150,toolkit.getScreenSize().height / 2 - 200,400,150);
+    }
+}
